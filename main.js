a0c4fae1fbd89daa3cae31eb8c25c435b68bee3c
Agustin Pereira <aguspereira@protonmail.com>
2023-12-14 02:51:40 -0300

    pase todo el juego a una sola funcion, ahora cada vez que ganamos se agrega un logo  a lal div de selecciones, la idea ahora es limitar manos por partida y agregar los puntajes al aside

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-const option = ['paper', 'rock', 'scissors'];
+
+
+const selectionPlayer = document.querySelector('#selections-player');
+const selectionComputer = document.querySelector('#selections-pc');
+let playerCount = 0;
+let computerCount = 0;
+let counter = 0;
+
+const option = ['paper', 'rock', 'scissor'];
 
 function getComputerChoice(){
     let choice = Math.floor((Math.random() * option.length) + 1); 
-    return option[choice - 1];
+    return option[choice-1];
 }
-
-
-let resultRandom = (Math.random() * 3);
 
 function playRound(playerSelection, computerSelection){
     /*
@@ -14,48 +19,48 @@ function playRound(playerSelection, computerSelection){
     Scissor beats paper 
     paper beats rock 
     */
-    let player = playerSelection.toLowerCase();
+    let computer = computerSelection;
 
     let winner = ""; 
-    if(player == option[0]){
+
+    let result = [];
+    if(playerSelection == option[0]){
         winner = (computerSelection == option[0]) ?   "Tie!" : 
                     (computerSelection == option[1]) ? "Player" : 
                         computerSelection == option[2] ? "Computer" : "";
+                        
     }
-    else if(player == option[1]){
+    else if(playerSelection == option[1]){
         winner = (computerSelection == option[1]) ?  "Tie!" : 
                     (computerSelection == option[2]) ? "Player" : 
                         computerSelection == option[0] ? "Computer" : "";
+                        
     }
-    else if(player == option[2]){ 
+    else if(playerSelection == option[2]){ 
         winner = (computerSelection == option[2]) ?  "Tie!" : 
                     (computerSelection == option[0]) ? "Player" : 
                         computerSelection == option[1] ? "Computer" : "";
+                        
     }
-    return winner;
+    /*we have to push in this orden: 0[winner], 1[player, 2[computer]] */
+    result.push(winner); 
+    result.push(playerSelection);
+    result.push(computer);
+
+    /*Game against machine here:*/
+    if (result[0] == "Player"){
+        playerCount++;
+        let newImg = new Image(50, 50);
+        newImg.src = `images/${result[1]}.png`;
+        selectionPlayer.appendChild(newImg);
+        
+    }else if(result[0] == "Computer"){
+        computerCount++;
+        let newImg = new Image(50, 50);
+        newImg.src = `images/${result[2]}.png`; 
+        selectionComputer.appendChild(newImg);
+    } 
+
 } 
 
 
-function game(number, playerSelection){
-    let playerCount = 0;
-    let computerCount = 0;
-
-
-    for (let index = 0; index < number-1; index++) {
-        let winner = playRound(playerSelection, getComputerChoice());
-        if (winner == "Player"){
-                playerCount++;
-        }else if(winner == "Computer"){
-                computerCount++;
-        }
-        //console.log(playerCount);
-    }
-    if(playerCount > computerCount){
-        console.log(`You win! you're score: ${playerCount}. Computer score: ${computerCount}.`);
-    }else if(computerCount > playerCount){
-        console.log(`You lose! :( \n  you're score: ${playerCount}. Computer score: ${computerCount}.`);
-    }else{
-        console.log('tie');
-    }
-
-}
