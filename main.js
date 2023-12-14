f81882bb0cdb50168b21a7128296fabdedcc593a
Agustin Pereira <aguspereira@protonmail.com>
2023-12-14 15:52:18 -0300

    0.0.1

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 
+const option = ['paper', 'rock', 'scissor'];
 
 const selectionPlayer = document.querySelector('#selections-player');
 const selectionComputer = document.querySelector('#selections-pc');
+const buttonPlay = document.querySelectorAll('.play-ui'); 
+const score = document.querySelector("div.score"); 
+const reset = document.querySelector("#reset"); 
+
+
+
+
 let playerCount = 0;
 let computerCount = 0;
-let counter = 0;
+let counter = Number(document.querySelector('#best-of').value);
 
-const option = ['paper', 'rock', 'scissor'];
+
 
 function getComputerChoice(){
     let choice = Math.floor((Math.random() * option.length) + 1); 
     return option[choice-1];
 }
 
+function createImage(url){
+    let newImg = new Image(50, 50);
+    newImg.src = `images/${url}.png`;
+    return newImg;
+}
+
 function playRound(playerSelection, computerSelection){
-    /*
-    Rock beats Scissor
-    Scissor beats paper 
-    paper beats rock 
-    */
-    let computer = computerSelection;
 
     let winner = ""; 
 
@@ -45,22 +53,59 @@ function playRound(playerSelection, computerSelection){
     /*we have to push in this orden: 0[winner], 1[player, 2[computer]] */
     result.push(winner); 
     result.push(playerSelection);
-    result.push(computer);
+    result.push(computerSelection);
 
     /*Game against machine here:*/
     if (result[0] == "Player"){
         playerCount++;
-        let newImg = new Image(50, 50);
-        newImg.src = `images/${result[1]}.png`;
+        let newImg = createImage(result[1]);
         selectionPlayer.appendChild(newImg);
         
     }else if(result[0] == "Computer"){
         computerCount++;
-        let newImg = new Image(50, 50);
-        newImg.src = `images/${result[2]}.png`; 
+        let newImg = createImage(result[2]);
         selectionComputer.appendChild(newImg);
     } 
 
 } 
+//game
+//let round = Number(document.querySelector('#best-of').value);
+let round = 0;
 
+buttonPlay.forEach(button => {
+    button.addEventListener('click', ()=>{
+        let bestOf = counter;
+        if(round < bestOf){
+            playRound(button.id, getComputerChoice());    
+            round++;
+            console.log(round);
+        }else{
+            if(playerCount > computerCount && score.innerHTML == ""){
+                let p = document.createElement('p');
+                p.innerHTML = `You win! :D  
+                \n score: ${playerCount}.`;
+                score.appendChild(p);
+            }else if(computerCount > playerCount && score.innerHTML == ""){
+                let p = document.createElement('p');
+                p.innerHTML = `You lose! :( 
+                    score: ${playerCount}.`;
+                score.appendChild(p);
+            }else if(playerCount == computerCount){
+                score.innerHTML = "<p> Tie!  </p>";
+                
+            }
+        }
+    });
+}); 
+
+reset.addEventListener("click", ()=>{
+    selectionPlayer.innerHTML = "";
+    selectionComputer.innerHTML = ""; 
+    counter = Number(document.querySelector('#best-of').value);
+    score.innerHTML = ""; 
+    playerCount = 0;
+    computerCount = 0;
+    round = 0; 
+
+});
 
